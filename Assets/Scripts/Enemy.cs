0dee53cb1a19bfa08c07624cb6f7d93232938d3d
kyhou <joelskill@gmail.com>
2017-03-03 03:41:46 -0300

    Final Commit
    Made following the tutorial from Unity for the creation of a roguelike
    game.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+    public int playerDamage;
+    public AudioClip enemyAttack1, enemyAttack2;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+    protected override void Start()
+    {
+        GameManager.instance.AddEnemyToList(this);
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        if (skipMove)
+        {
+            skipMove = false;
+            return;
+        }
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        skipMove = true;
+    }
+
+    public void MoveEnemy()
+    {
+        int xDir = 0, yDir = 0;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            yDir = transform.position.y > transform.position.y ? 1 : -1;
+        }
+        else
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Player hitPlayer = component as Player;
+
+        animator.SetTrigger("EnemyAttack");
+
+        SoundManager.instance.RandomizeSfx(enemyAttack1, enemyAttack2);
+
+        hitPlayer.LoseFood(playerDamage);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RoguelikeTutorial
+
+Made following the tutorial from Unity for the creation of a roguelike game.
+Unity 5.5.1f1
+No aditional modifications made.
